42ee04c006f3f25e90719fd84809ecf9459eadf8
ilay2k18 <65078471+ilay2k18@users.noreply.github.com>
2020-05-11 20:24:14 -0700

    new local settings

--- ilay_portfolio/settings.py
+++ ilay_portfolio/settings.py
@@ -124,3 +124,8 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("looks like no local file. its a production file")
